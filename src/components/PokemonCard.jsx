2b41a2269e100fd44404ea9e4d0aedd96e6382a8
Daniela Tricolici <dtricolici12345@gmail.com>
2023-12-12 16:39:15 +0100

    added components.2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
+import "../App.jsx";
+
+
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
 
 function PokemonCard() {
+
+    const pokemon = pokemonList[0]; 
+
     return (
-      <figure className="card">
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="Bulbasaur"
-          className="card-img"
-        />
-        <figcaption>Bulbasaur</figcaption>
-      </figure>
+        <figure className="card">
+            {pokemon.imgSrc ? (
+                <img
+                    src={pokemon.imgSrc}
+                    alt={pokemon.name}
+
+                />
+            ) : (
+                <p>???</p>
+            )}
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
     );
-  }
-  
-  export default PokemonCard;
+}
+
+export default PokemonCard;
