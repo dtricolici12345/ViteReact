58e7f2642a265588379dc8706fde6341e05f4b25
Daniela Tricolici <dtricolici12345@gmail.com>
2023-12-13 17:10:01 +0100

    added state.1

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-function PokemonCard(props) {
-    const { pokemon } = props;
 
-    return (
-        <div className="pokemon-card">
-            {pokemon.imgSrc ? (
-                <img src={pokemon.imgSrc} alt={pokemon.name} />
-            ) : (
-                <p>???</p>
-            )}
-            <h2>{pokemon.name}</h2>
-        </div>
-    );
+function PokemonCard(props) {
+	const { pokemon } = props;
+
+	return (
+		<div className="pokemon-card">
+			<h2>{pokemon.name}</h2>
+			{pokemon.imgSrc ? (
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
+			) : (
+				<p>???</p>
+			)}
+		</div>
+	);
 }
 
 PokemonCard.propTypes = {
-    pokemon: PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        imgSrc: PropTypes.string,
-    }).isRequired,
+	pokemon: PropTypes.shape({
+		name: PropTypes.string.isRequired,
+		imgSrc: PropTypes.string,
+	})
 };
+
 
 export default PokemonCard;
